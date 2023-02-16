95d3cf28ac84507e37f3a06fc187c087c6a7b32f
B4Lee <iqbalanggoro48@gmail.com>
2023-02-16 18:37:36 +0700

    feat: iterate on array of objects in projects

---
+++ data/MyProjects.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        img: require("assets/img/kramatmotor.png"),
+        title: 'kramatmotor.com',
+        path: 'https://kramatmotor.com',
+        source: 'https://kramatmotor.com',
+        description: 'Company Profiles for PT Kramat Motor',
+        stack: 'HTML | CSS | JAVASCRIPT | BOOTSTRAP',
+    },
+    {
+        img: require("assets/img/tokorackserver.png"),
+        title: 'Toko Rack Server',
+        path: 'https://tokorackserver.com',
+        source: 'https://tokorackserver.com',
+        description: 'E-Commerce Website for PT Trikomindo Karunia Utama',
+        stack: 'WORDPRESS | PHP | BOOTSTRAP | WOOCOMMERCE'
+    },
+    {
+        img: require("assets/img/airitservice.png"),
+        title: 'AIR IT SERVICE Landing Page',
+        path: 'https://airitservice.web.id/',
+        source: 'https://airitservice.web.id/',
+        description: 'Landing page for AIR IT Service',
+        stack: 'HTML | CSS | JAVASCRIPT | BOOTSTRAP',
+    },
+]
