06647d164e6a28939a00adf2d5acd432e11b7248
B4Lee <iqbalanggoro48@gmail.com>
2023-05-25 13:38:15 +0700

    docs: :memo: update bio

--- components/NuxtHome.vue
+++ components/NuxtHome.vue
@@ -6,13 +6,14 @@
         <img alt="Party" src="../static/icon.png" class="absolute inset-0 h-full w-full object-cover" />
       </div>
       <div class="lg:py-24">
-        <h2 class="text-3xl font-bold sm:text-4xl dark:text-white">Hi, I'm Iqbal! </h2>
+        <h2 class="text-3xl font-bold sm:text-4xl dark:text-white">Hi, I'm Iqbal!  👋 </h2>
         <p class="mt-4 noselect text-md md:text-lg md:text-left max-w-[550px] dark:text-white">
-        A Software Developer from Indonesia 🇮🇩 <br> I use this site to document everything I learned from programming, my experience, and random thoughts.
+        A Web Developer from Indonesia 🇮🇩 <br> I use this site to document everything I learned from programming, my experience, and random thoughts.
           <!-- If you have any questions, <a class="underline relative z-10 font-semibold" href="/contact">contact me</a> -->
         </p>
+        <!-- <p class="text-center dark:text-white md:text-left max-w-[550px]">Checkout my <a class="underline relative z-10 font-semibold" href="/projects">past works</a> or learn more <a class="underline relative z-10 font-semibold" href="/about">about me</a>.</p> -->
         <NuxtLink to="/about" class="mt-8 inline-flex items-center rounded border border-green-600 bg-green-600 px-8 py-3 text-white hover:text-white focus:outline-none focus:ring active:text-green-500">
-          <span class="text-sm font-medium">About me ➡</span>
+          <span class="text-sm font-medium">About me 👉</span>
         </NuxtLink>
       </div>
     </div>
