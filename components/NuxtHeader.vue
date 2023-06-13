8aee16d287148edefa735875fc60f7130ae5bda7
B4Lee <iqbalanggoro48@gmail.com>
2023-06-13 17:00:38 +0700

    Fix: Icon elements do not have [alt],height and witdth attributes
    also fix medium responsiveness

--- components/NuxtHeader.vue
+++ components/NuxtHeader.vue
@@ -2,8 +2,8 @@
   <header aria-label="Site Header" class="sticky z-50">
     <div class="bg-white dark:bg-slate-800 shadow-sm pt-8">
       <div class="flex mx-auto items-center justify-between max-w-4xl">
-        <div class="lg:flex items-center pr-4 bg-white dark:bg-slate-800 hidden sm:block">
-          <img src="../static/icon.png" class="h-8" />
+        <div class="lg:flex items-center pr-4 bg-white dark:bg-slate-800 hidden md:hidden sm:block">
+          <img src="../static/icon.png" class="h-8" alt="nuxtIcon" height="2rem" max-width="100%" />
           <NuxtLink
             class="font-semibold items-center justify-center text-center text-xl 4 px-2 dark:hover:bg-light transition duration-200 ease-in-out"
             to="/">Iqbal Anggoro
@@ -19,7 +19,7 @@
           <NuxtLink class="text-gray-900 dark:text-white hover:bg-gray-600" to="/blog">Blog</NuxtLink>
           <NuxtLink class="text-gray-900 dark:text-white hover:bg-gray-600" to="/projects">Projects</NuxtLink>
         </nav>
-        <div class="items-center gap-4 lg:flex hidden sm:block">
+        <div class="items-center gap-4 lg:flex hidden md:hidden sm:block">
           <DarkToggle />
         </div>
       </div>
