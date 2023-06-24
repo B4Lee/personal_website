f3199514462cce1cd204b89fc8685a1f29488cb5
B4Lee <iqbalanggoro48@gmail.com>
2023-06-24 23:56:52 +0700

    fix: project stacks to array
    change some title

--- components/ProjectsCard.vue
+++ components/ProjectsCard.vue
@@ -10,8 +10,9 @@
             <h1 class="font-bold text-xl"><a rel="noopener noreferrer" target="_blank" :href="`${project.path}`">{{ project.title }}</a></h1>
             <br>
             <p class="mt-2 text-dark dark:text-white">{{ project.description }}</p>
-            <div class="flex flex-wrap mt-2 space-x-2">
-              <div class="rounded-sm text-xs uppercase px-1  text-white font-bold bg-green-500 dark:bg-green-500">{{project.stack}}</div>
+            <br />
+            <div class="flex flex-wrap mt-6 space-x-2">
+              <div v-for="(stack, i) in project.stack" :key="i" class="rounded-sm mb-2 text-xs uppercase px-1  text-white font-bold bg-dark-green dark:bg-dark-green">{{stack}}</div>
             </div>
             <div class="flex mt-8 space-x-4">
               <a class="font-semibold text-lg hover:underline" rel="noopener noreferrer" target="_blank" :href="`${project.path}`"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" class="inline" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path></svg> Visit Website</a>
--- data/MyProjects.js
+++ data/MyProjects.js
@@ -5,22 +5,22 @@ export const projects = [
         path: 'https://kramatmotor.com',
         source: 'https://kramatmotor.com',
         description: 'Company Profiles for PT Kramat Motor',
-        stack: 'HTML | CSS | JAVASCRIPT | BOOTSTRAP',
+        stack: ["HTML", "CSS", "JAVASCRIPT", "BOOTSTRAP"],
     },
     {
         img: require("assets/img/tokorackserver.png"),
-        title: 'Toko Rack Server',
+        title: 'tokorackserver.com',
         path: 'https://tokorackserver.com',
         source: 'https://tokorackserver.com',
         description: 'E-Commerce Website for PT Trikomindo Karunia Utama',
-        stack: 'WORDPRESS | PHP | BOOTSTRAP | WOOCOMMERCE'
+        stack: ['WORDPRESS', 'PHP', 'BOOTSTRAP', 'WOOCOMMERCE'],
     },
     {
         img: require("assets/img/airitservice.png"),
-        title: 'AIR IT SERVICE Landing Page',
+        title: 'airitservice.web.id',
         path: 'https://airitservice.web.id/',
         source: 'https://airitservice.web.id/',
         description: 'Landing page for AIR IT Service',
-        stack: 'HTML | CSS | JAVASCRIPT | BOOTSTRAP',
+        stack: ["HTML", "CSS", "JAVASCRIPT", "BOOTSTRAP"],
     },
 ]
